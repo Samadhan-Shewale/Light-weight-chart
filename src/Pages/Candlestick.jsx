936f2e724818c60a1d51cba175bc9b9d0a17a7bc
Samadhan Shewale <samadhanshewale512@gmail.com>
2026-04-06 11:30:28 +0530

    Basic-customization applyed

--- src/Pages/Candlestick.jsx
+++ src/Pages/Candlestick.jsx
@@ -1,45 +1,84 @@
 import { createChart, ColorType, CandlestickSeries } from 'lightweight-charts';
 import { useEffect, useRef } from 'react';
+import data from '../data/data';
 
 function Candlestick() {
   const chartContainerRef = useRef(null);
 
   useEffect(() => {
-    const data = [
-        { time: 1642427876, open: 10.00, high: 10.63, low: 9.49, close: 9.55 },
-        { time: 1642514276, open: 9.55, high: 10.30, low: 9.42, close: 9.94 },
-        { time: 1642600676, open: 9.94, high: 10.17, low: 9.92, close: 9.78 },
-        { time: 1642687076, open: 9.78, high: 10.59, low: 9.18, close: 9.51 },
-        { time: 1642773476, open: 9.51, high: 10.46, low: 9.10, close: 10.17 },
-        { time: 1642859876, open: 10.17, high: 10.96, low: 10.16, close: 10.47 },
-        { time: 1642946276, open: 10.47, high: 11.39, low: 10.40, close: 10.81 },
-        { time: 1643032676, open: 10.81, high: 11.60, low: 10.30, close: 10.75 },
-        { time: 1643119076, open: 10.75, high: 11.60, low: 10.49, close: 10.93 },
-        { time: 1643205476, open: 10.93, high: 11.53, low: 10.76, close: 11.40 }
-      ];
-    const chartOptions = { 
-      layout: { 
-        textColor : 'black', 
-         background: { type: ColorType.Solid, color: 'white' }, 
-      } };
-    const chart = createChart( chartContainerRef.current, {
-      ...chartOptions,
-      width:chartContainerRef.current.clientWidth,
-      height: 500,
-    } );
+    const chartOptions = {
+        layout: {
+            background: { type: ColorType.Solid, color: '#222' },
+            textColor: '#DDD',
+        },
+        grid: {
+            vertLines: { color: '#444' },
+            horzLines: { color: '#444' },
+        },
+        width:chartContainerRef.current.clientWidth,
+        height: 500,
+        localization: {
+          priceFormatter: (price)=> {
+            const myPrice =  Intl.NumberFormat( "en-US", {
+                style : "currency",
+                currency : "USD"
+            }).format(price) 
 
-    const candlestickSeries = chart.addSeries( CandlestickSeries, { 
-      upColor: '#26a69a', 
-      downColor: '#ef5350', 
-      borderVisible: false, 
-      wickUpColor: '#26a69a', 
-      wickDownColor: '#ef5350' 
+            return myPrice
+          },
+        },
+    }; 
+    
+    const chart = createChart( chartContainerRef.current );
+    chart.applyOptions( chartOptions );
+    
+    // Setting the border color for the vertical axis
+    chart.priceScale("right").applyOptions({
+        borderColor: '#71649C',
+        visible : true
     });
 
+    // chart.priceScale("left").applyOptions({
+    //     borderColor: '#71649C',
+    //     visible : true
+    // });
+
+    // Setting the border color for the horizontal axis
+    chart.timeScale().applyOptions({
+        borderColor: '#71649C',
+        rightOffset : 30,
+        barSpacing: 10,
+        minBarSpacing:3,
+        fixLeftEdge : true,
+        timeVisible : true
+    });
+
+    const candlestickSeries = chart.addSeries( CandlestickSeries );
+    const seriesOptions = { 
+      upColor: 'rgb(54, 116, 217)', 
+      downColor:'rgb(225, 50, 85)',
+      borderVisible: false, 
+      wickUpColor: 'rgb(54, 116, 217)',
+      wickDownColor: 'rgb(225, 50, 85)',
+    }
+
+    chart.applyOptions( seriesOptions )
+
     candlestickSeries.setData(data);
+
+    const handleResize = ()=>{
+      chart.applyOptions({
+        width : chartContainerRef.current.clientWidth,
+      })
+    }
+    window.addEventListener('resize', handleResize );
     
-    return () => chart.remove(); // cleanup
-  }, []);
+    return () => {
+      window.removeEventListener('resize', handleResize );
+      chart.remove();
+    }; // cleanup
+
+  }, []); 
 
   return <div ref={chartContainerRef} />;
 }
